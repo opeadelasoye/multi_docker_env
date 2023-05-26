8f0e2ded8ccc570c48233ad564f44fd72ddfe748
Ope Adelasoye <on861917@dal.ca>
2023-05-26 14:58:27 -0300

    Application 2

--- app2/app.py
+++ app2/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return 'Hello! This is application 2'
+	resp = requests.get("http://app1:5000")
+	return resp.text
